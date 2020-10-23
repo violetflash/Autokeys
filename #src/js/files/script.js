6a55259296e893df41ffd699a404c8a62b660260
Alex Grobovsky <ktotaam@mail.ru>
2020-10-23 20:06:22 +0500

    card 100%

---
+++ #src/js/files/script.js
@@ -0,0 +1,12 @@
+///////////////
+///Add to favorites button
+///////////////
+
+let favorites = document.querySelectorAll('.card__add-to-favorites');
+for (let i = 0; i < favorites.length; i++) {
+    const favoriteBtn = favorites[i]
+
+    favoriteBtn.addEventListener('click', function(e) {
+        favoriteBtn.classList.toggle("added");
+    });
+}
