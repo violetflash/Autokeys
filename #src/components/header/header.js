f10a6b838a3c759eb1c244d20b0c2126584490fa
Alex Grobovsky <ktotaam@mail.ru>
2020-10-30 00:16:11 +0500

    adaptive header process-90%

--- #src/components/header/header.js
+++ #src/components/header/header.js
@@ -8,24 +8,35 @@ $('.top-header__select').select2({
 
 
 //Header NAV menu switch active
-let navLinks = document.querySelectorAll('.bottom-header__link');
-
-for (let i = 0; i < navLinks.length; i++) {
-    let navLink = navLinks[i];
-
-    navLink.addEventListener('click', function(e) {
-
-        for (let i = 0; i < navLinks.length; i++) {
-            navLinks[i].classList.remove('_active');
-        }
-        this.classList.add('_active');
-    });
-}
+// let navLinks = document.querySelectorAll('.bottom-header__link');
+//
+// for (let i = 0; i < navLinks.length; i++) {
+//     let navLink = navLinks[i];
+//
+//     navLink.addEventListener('click', function(e) {
+//
+//         for (let i = 0; i < navLinks.length; i++) {
+//             navLinks[i].classList.remove('_active');
+//         }
+//         this.classList.add('_active');
+//     });
+// }
 
 // Header NAV dropdown menu open
-let navDropdownBtn = document.querySelector('.bottom-header__dropdown');
 
-navDropdownBtn.addEventListener('click', function(e) {
-    navDropdownBtn.classList.toggle('_active');
-});
+$('.bottom-header__dropdown-btn').on('click', function () {
+    $(this).toggleClass('_active');
+    $('.bottom-header__dropdown-list').animate({'height': 'toggle'}, 300)
+})
 
+$(document).click(function (e) {
+    e.stopPropagation();
+    var container = $('.bottom-header__dropdown');
+
+    //check if the clicked area is dropDown or not
+    if (container.has(e.target).length === 0) {
+        $('.bottom-header__dropdown-list').slideUp(300);
+        $('.bottom-header__dropdown-btn').removeClass('_active')
+
+    }
+})
