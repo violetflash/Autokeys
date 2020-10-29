8ece3b38d5fe30f8ac1a8c5843c4934fd49409c2
Alex Grobovsky <ktotaam@mail.ru>
2020-10-29 12:47:48 +0500

    header adaptive process

--- #src/components/header/header.js
+++ #src/components/header/header.js
@@ -1,11 +1,31 @@
+// City choice select
 $('.top-header__select').select2({
     width: "150px",
     minimumResultsForSearch: Infinity,
-    dropdownCssClass: "header-dropdown",
+    dropdownCssClass: "city-dropdown",
 });
 
-$('.select2-selection').css('border-radius', '0px')
-$('.select2-container').children().css('border-radius', '0px')
 
 
+//Header NAV menu switch active
+let navLinks = document.querySelectorAll('.bottom-header__link');
+
+for (let i = 0; i < navLinks.length; i++) {
+    let navLink = navLinks[i];
+
+    navLink.addEventListener('click', function(e) {
+
+        for (let i = 0; i < navLinks.length; i++) {
+            navLinks[i].classList.remove('_active');
+        }
+        this.classList.add('_active');
+    });
+}
+
+// Header NAV dropdown menu open
+let navDropdownBtn = document.querySelector('.bottom-header__dropdown');
+
+navDropdownBtn.addEventListener('click', function(e) {
+    navDropdownBtn.classList.toggle('_active');
+});
 
