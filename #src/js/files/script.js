7fa20fab0e125142b7943adbd2723299b8b30197
Alex Grobovsky <ktotaam@mail.ru>
2020-11-03 12:52:17 +0500

    added about section + adaptive

--- #src/js/files/script.js
+++ #src/js/files/script.js
@@ -11,9 +11,31 @@ for (let i = 0; i < favorites.length; i++) {
     });
 }
 
+///////
+/// PHONE MASK
+//////
+
 let selector = document.querySelectorAll('input[type="tel"]');
 
 let im = new Inputmask("+7 (999) 999-99-99");
 
 im.mask(selector);
+
+
+//////////////////
+/// Validation
+/////////////////
+
+let validateForms = function (selector, rules) {
+    new window.JustValidate(selector, {
+        rules: rules,
+    })
+}
+
+validateForms('.call-request', {
+    tel: {
+        required: true,
+    }
+})
+
 
